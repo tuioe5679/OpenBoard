def67375659fedcab3948b966453b7118a8d6732
tuioe <qodlgks123@naver.com>
2025-03-29 00:35:43 +0900

    Initial commit

---
+++ src/main/java/com/domain/openboard/OpenBoardApplication.java
@@ -0,0 +1,13 @@
+package com.domain.openboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/domain/openboard/OpenBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.openboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

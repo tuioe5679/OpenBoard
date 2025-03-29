5f4181377c521e3ea33e3b5a03e07f18deac28d8
tuioe <qodlgks123@naver.com>
2025-03-29 18:33:22 +0900

    feat: 생성일 / 수정일 자동 기록 기능을 위한 메인 클래스에 어노테이션 추가

--- src/main/java/com/domain/openboard/OpenBoardApplication.java
+++ src/main/java/com/domain/openboard/OpenBoardApplication.java
@@ -2,12 +2,13 @@ package com.domain.openboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OpenBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenBoardApplication.class, args);
     }
-
 }

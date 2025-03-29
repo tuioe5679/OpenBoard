3cbe9cdb51b2d7b74d1ea10a04f86cb4d1caf404
tuioe <qodlgks123@naver.com>
2025-03-29 21:13:00 +0900

    refactor: SecurityConfig 필요한 import문 추가

--- src/main/java/com/domain/openboard/config/SecurityConfig.java
+++ src/main/java/com/domain/openboard/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.domain.openboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {

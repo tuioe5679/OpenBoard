59ce60acce512f64e432eb0cdcccd6a8e24b3863
tuioe <qodlgks123@naver.com>
2025-03-29 20:38:20 +0900

    feat: 게시글 전체 목록 조회 API 구현

--- src/main/java/com/domain/openboard/controller/PostController.java
+++ src/main/java/com/domain/openboard/controller/PostController.java
@@ -1,15 +1,14 @@
 package com.domain.openboard.controller;
 
 import com.domain.openboard.domain.Post;
+import com.domain.openboard.dto.PostListResponseDto;
 import com.domain.openboard.dto.PostRequestDto;
 import com.domain.openboard.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api") // 전체 기본 경로
@@ -24,5 +23,16 @@ public class PostController {
         Post post = postService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(post);
+    }
+
+    // 게시글 목록 조회 API
+    @GetMapping("/posts")
+    public ResponseEntity<List<PostListResponseDto>> getAllPosts() {
+        List<PostListResponseDto> postListDto = postService.findAll()
+                .stream()                               // 컬렉션(List)를 Stream 객체로 변환
+                .map(PostListResponseDto::new)          // 각 Post 객체를 PostListResponseDto로 변환
+                .toList();                              // 변환된 요소들을 새로운 리스트(List<PostListResponseDto>)로 수집
+                                                        // List<Post> -> Stream -> PostListResponseDto -> List<PostListResponseDto>
+        return ResponseEntity.ok().body(postListDto);
     }
 }

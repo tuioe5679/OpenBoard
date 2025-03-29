b0045923f1d322c225b4a7b367c7d0e8dad80f30
tuioe <qodlgks123@naver.com>
2025-03-29 20:21:57 +0900

    feat: PostService 게시글 전체 조회 로직 구현

--- src/main/java/com/domain/openboard/service/PostService.java
+++ src/main/java/com/domain/openboard/service/PostService.java
@@ -7,6 +7,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor // final이 붙거나 @NoNull이 붙은 필드의 생성자 추가
 @Transactional // 클래스 전체 트랜잭션 적용
@@ -14,7 +16,13 @@ public class PostService {
 
     private final PostRepository postRepository;
 
+    // 게시글 작성
     public Post save(PostRequestDto dto){
         return postRepository.save(dto.toEntity());
+    }
+
+    // 게시글 전체 조회
+    public List<Post> findAll() {
+        return postRepository.findAll();
     }
 }

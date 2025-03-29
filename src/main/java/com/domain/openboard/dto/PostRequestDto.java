17c22fb51e4286f4dbde5bae48bfd80ffe2053d9
tuioe <qodlgks123@naver.com>
2025-03-29 17:29:14 +0900

    feat: 게시글 작성 요청 DTO 클래스 생성

---
+++ src/main/java/com/domain/openboard/dto/PostRequestDto.java
@@ -0,0 +1,24 @@
+package com.domain.openboard.dto;
+
+import com.domain.openboard.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor// 모든 필드 값을 파파미터로 받는 생성자
+@Getter
+public class PostRequestDto {
+
+    private String title;
+    private String content;
+    private String name;
+
+    public Post toEntity() { // 생성자를 통해 객체 생성
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .name(name)
+                .build();
+    }
+}

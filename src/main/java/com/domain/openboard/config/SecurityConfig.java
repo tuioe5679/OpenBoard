f50e543ce8742038ccd7d2c383975e961b7ce000
tuioe <qodlgks123@naver.com>
2025-03-29 21:12:14 +0900

    Revert "refactor: import 추가"
    This reverts commit f8345277073a4ac55681040754b73a016d472b34.

--- src/main/java/com/domain/openboard/config/SecurityConfig.java
+++ src/main/java/com/domain/openboard/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.domain.openboard.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {

7b3848ec5b7380a5d76e3c96d8cbfbb99f2d25b3
tuioe <qodlgks123@naver.com>
2025-03-30 14:54:53 +0900

    feat: 에러 응답을 위한 ErrorResponse DTO 생성

---
+++ src/main/java/com/domain/openboard/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.domain.openboard.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+// 에러 메시지 응답용 객체
+public class ErrorResponse {
+
+    private String message;
+    private String code;
+}

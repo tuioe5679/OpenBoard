b322acebdd526c7105394aba994e1307c65b9d6f
tuioe <qodlgks123@naver.com>
2025-03-30 16:03:42 +0900

    refactor: GlobalExceptionHandler 하드코딩 제거 및 ErrorCode enum 적용

--- src/main/java/com/domain/openboard/error/GlobalExceptionHandler.java
+++ src/main/java/com/domain/openboard/error/GlobalExceptionHandler.java
@@ -16,15 +16,17 @@ public class GlobalExceptionHandler {
     // DTO 유효성 검사 실패 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
+
+        ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
         String message = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .findFirst()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .orElse("잘못된 요청입니다.");
+                .orElse(errorCode.getMessage());
 
         return ResponseEntity.badRequest()
-                .body(new ErrorResponse("VALIDATION_ERROR", message));
+                .body(new ErrorResponse(errorCode.getCode(),message));
     }
 
     // 커스텀 예외 처리
@@ -40,7 +42,8 @@ public class GlobalExceptionHandler {
     // 그외 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("INTERNAL_SERVER_ERROR", ex.getMessage()));
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(errorCode.getCode(), errorCode.getMessage()));
     }
 }

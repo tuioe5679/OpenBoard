20d5c9c1e3ade3f7846d09c28cb656d641ee7704
tuioe <qodlgks123@naver.com>
2025-04-03 16:27:27 +0900

    test: PostController 유효성 검증 실패 테스트 추가

--- src/test/java/com/domain/openboard/controller/PostControllerTest.java
+++ src/test/java/com/domain/openboard/controller/PostControllerTest.java
@@ -248,4 +248,94 @@ class PostControllerTest {
         Assertions.assertThat(post.getTitle()).isEqualTo(updateTitle);
         Assertions.assertThat(post.getContent()).isEqualTo(updateContent);
     }
+
+    @DisplayName("addPost: 제목이 비어있으면 VALIDATION_ERROR 발생")
+    @Test
+    public void addPostBlankTitleValidationError() throws Exception {
+        // Given (테스트 준비)
+
+        // 게시글 추가에 필요한 요청 객체를 만들기
+        final String url = "/api/posts";
+        final String title = "";
+        final String content = "content";
+        final String name = "name";
+        final String password = "password";
+        final PostRequestDto dto = new PostRequestDto(title,content,name,password);
+
+        // 객체 JSON으로 직렬화 (DTO를 JSON 문자열로 변환)
+        final String requestBody = objectMapper.writeValueAsString(dto);
+
+        // when (테스트 진행)
+
+        // JSON 형태로 POST 요청 전송
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)  // Content-Type : application_json_value
+                .content(requestBody));                         // 요청 본문
+
+        // then (테스트의 결과를 검증)
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+                .andExpect(jsonPath("$.message").value("제목은 필수입니다."));
+    }
+
+    @DisplayName("addPost: 내용이 비어있으면 VALIDATION_ERROR 발생")
+    @Test
+    public void addPostBlankContentValidationError() throws Exception {
+        // Given (테스트 준비)
+
+        // 게시글 추가에 필요한 요청 객체를 만들기
+        final String url = "/api/posts";
+        final String title = "title";
+        final String content = "";
+        final String name = "name";
+        final String password = "password";
+        final PostRequestDto dto = new PostRequestDto(title,content,name,password);
+
+        // 객체 JSON으로 직렬화 (DTO를 JSON 문자열로 변환)
+        final String requestBody = objectMapper.writeValueAsString(dto);
+
+        // when (테스트 진행)
+
+        // JSON 형태로 POST 요청 전송
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)  // Content-Type : application_json_value
+                .content(requestBody));                         // 요청 본문
+
+        // then (테스트의 결과를 검증)
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+                .andExpect(jsonPath("$.message").value("내용은 필수입니다."));
+    }
+
+    @DisplayName("addPost: 내용이 비어있으면 VALIDATION_ERROR 발생")
+    @Test
+    public void addPostBlankNameValidationError() throws Exception {
+        // Given (테스트 준비)
+
+        // 게시글 추가에 필요한 요청 객체를 만들기
+        final String url = "/api/posts";
+        final String title = "title";
+        final String content = "content";
+        final String name = "a".repeat(21);
+        final String password = "password";
+        final PostRequestDto dto = new PostRequestDto(title,content,name,password);
+
+        // 객체 JSON으로 직렬화 (DTO를 JSON 문자열로 변환)
+        final String requestBody = objectMapper.writeValueAsString(dto);
+
+        // when (테스트 진행)
+
+        // JSON 형태로 POST 요청 전송
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)  // Content-Type : application_json_value
+                .content(requestBody));                         // 요청 본문
+
+        // then (테스트의 결과를 검증)
+        resultActions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("VALIDATION_ERROR"))
+                .andExpect(jsonPath("$.message").value("작성자는 20자 이하로 작성해주세요."));
+    }
 }

badc6f9bc3110a192b2bacd8f823668240a06482
tuioe <qodlgks123@naver.com>
2025-04-07 21:55:26 +0900

    feat: DTO Swagger 적용

--- src/main/java/com/domain/openboard/dto/PostListResponseDto.java
+++ src/main/java/com/domain/openboard/dto/PostListResponseDto.java
@@ -1,13 +1,19 @@
 package com.domain.openboard.dto;
 
 import com.domain.openboard.domain.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class PostListResponseDto {
 
+    @Schema(description = "게시글 제목",example = "제목")
     private final String title;
+
+    @Schema(description = "게시글 내용",example = "내용")
     private final String content;
+
+    @Schema(description = "게시글 작성자",example = "이름")
     private final String name;
 
     public PostListResponseDto(Post post) {

92267017f97cde7f5387ceaf7971cb6198f22d41
tuioe <qodlgks123@naver.com>
2025-05-09 20:37:13 +0900

    fix: contextLoads 테스트 실패 원인인 PropertySource 주석 처리
    - env.properties 파일 미존재로 인해 contextLoads 테스트 실패
    - CI 환경에서 빌드 실패 방지를 위해 PropertyConfig 클래스의 PropertySource 주석 처리

--- src/main/java/com/domain/openboard/config/PropertyConfig.java
+++ src/main/java/com/domain/openboard/config/PropertyConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 
-@Configuration      // 설정 클래스 Spring이 클래스를 Configuration으로 인식하여 Bean 등록 등을 처리
-@PropertySources({  // 여러 개의 프로퍼티 파일을 한 번에 불러올 수 있게 해주는 애너테이션
-                    // classpath 하위의 properties/env.properties 파일을 불러오겠다는 설정
-        @PropertySource("classpath:properties/env.properties")
-})
+//@Configuration      // 설정 클래스 Spring이 클래스를 Configuration으로 인식하여 Bean 등록 등을 처리
+//@PropertySources({  // 여러 개의 프로퍼티 파일을 한 번에 불러올 수 있게 해주는 애너테이션
+//                    // classpath 하위의 properties/env.properties 파일을 불러오겠다는 설정
+//        @PropertySource("classpath:properties/env.properties")
+//})
 public class PropertyConfig {
 }
